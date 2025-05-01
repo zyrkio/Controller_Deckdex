9d9f3072b042ccaa9314e153db9d209cecfdd400
zyrkio <deddoame@gmail.com>
2025-05-01 10:36:57 +0000

    final clean setup of Docker&Ubuntu&Extensions

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
 }
-
+  
+  // the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(1000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(1000);                      // wait for a second
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+  
